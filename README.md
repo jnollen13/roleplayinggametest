221d0fd590a710e1f02139b037c7ca9a7b04b733
jnollen13 <jerome.nollen@gmail.com>
2025-02-08 05:35:19 -0500

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Open this page at [https://jnollen13.github.io/roleplayinggametest/](https://jnollen13.github.io/roleplayinggametest/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/jnollen13/roleplayinggametest** and import
+
+## Edit this project ![Build status badge](https://github.com/jnollen13/roleplayinggametest/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/jnollen13/roleplayinggametest** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/jnollen13/roleplayinggametest/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,271 @@
+namespace SpriteKind {
+    export const talkity = SpriteKind.create()
+    export const Jodim = SpriteKind.create()
+    export const villager1 = SpriteKind.create()
+    export const villager2 = SpriteKind.create()
+    export const vendor = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level8`)
+    tiles.placeOnTile(playersprite, tiles.getTileLocation(7, 20))
+    myEnemy = sprites.create(assets.image`snake`, SpriteKind.Enemy)
+    animation.runImageAnimation(
+    myEnemy,
+    assets.animation`seaL`,
+    200,
+    true
+    )
+    myEnemy.follow(playersprite, 75)
+    statusbar2 = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
+    statusbar2.attachToSprite(myEnemy)
+    statusbar2.max = 13
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (start == 1) {
+        pause(100)
+        scene.setBackgroundImage(assets.image`bg0`)
+        pause(78)
+        start = 2
+        scene.setBackgroundImage(assets.image`Jodim Studios`)
+        sprites.destroy(textSprite)
+        textSprite2.y += -6
+        textSprite2.setScale(0.1, ScaleAnchor.Middle)
+        textSprite2.setText("Jodim Studios")
+        for (let index = 0; index < 225; index++) {
+            pause(1)
+            textSprite2.changeScale(0.01, ScaleAnchor.Middle)
+        }
+        textSprite2.setScale(2, ScaleAnchor.Middle)
+        pause(5000)
+        for (let index = 0; index < 234; index++) {
+            pause(1)
+            textSprite2.changeScale(-0.01, ScaleAnchor.Middle)
+        }
+        pause(3)
+        scene.setBackgroundImage(assets.image`bg1`)
+        pause(1000)
+        tiles.setCurrentTilemap(tilemap`level2`)
+        playersprite.setImage(assets.image`plaas`)
+        tiles.placeOnRandomTile(playersprite, sprites.dungeon.floorLight5)
+        scene.cameraFollowSprite(playersprite)
+        controller.moveSprite(playersprite)
+        start = 0
+        statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+        statusbar.attachToSprite(playersprite)
+        statusbar.setLabel("HP")
+        statusbar.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (start == 0 && swap == 0) {
+        animation.runImageAnimation(
+        playersprite,
+        assets.animation`plaaaL`,
+        200,
+        true
+        )
+    } else if (start == 0 && swap == 1) {
+        animation.runImageAnimation(
+        Jodimsprite,
+        assets.animation`pjaaL`,
+        200,
+        true
+        )
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    tiles.setTileAt(tiles.getTileLocation(9, 6), sprites.dungeon.floorLight2)
+    tiles.setWallAt(tiles.getTileLocation(9, 6), false)
+    tiles.setTileAt(tiles.getTileLocation(9, 5), sprites.dungeon.floorLight2)
+    tiles.setWallAt(tiles.getTileLocation(9, 5), false)
+    tiles.setTileAt(tiles.getTileLocation(8, 5), sprites.dungeon.floorLight2)
+    tiles.setWallAt(tiles.getTileLocation(8, 5), false)
+    tiles.setTileAt(tiles.getTileLocation(8, 4), sprites.dungeon.floorLight2)
+    tiles.setWallAt(tiles.getTileLocation(8, 4), false)
+    tiles.setTileAt(tiles.getTileLocation(7, 4), sprites.dungeon.floorLight2)
+    tiles.setWallAt(tiles.getTileLocation(7, 4), false)
+    tiles.setWallAt(tiles.getTileLocation(7, 3), false)
+    tiles.setTileAt(tiles.getTileLocation(7, 3), sprites.dungeon.floorLight2)
+    tiles.setWallAt(tiles.getTileLocation(6, 3), false)
+    tiles.setTileAt(tiles.getTileLocation(6, 3), sprites.dungeon.floorLight2)
+    if (open == 0) {
+        game.showLongText("come on in!", DialogLayout.Bottom)
+        open = 1
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    if (start == 0 && swap == 0) {
+        animation.stopAnimation(animation.AnimationTypes.ImageAnimation, playersprite)
+        playersprite.setImage(assets.image`asset0`)
+    } else if (start == 0 && swap == 1) {
+        animation.stopAnimation(animation.AnimationTypes.ImageAnimation, playersprite)
+        animation.stopAnimation(animation.AnimationTypes.ImageAnimation, Jodimsprite)
+        Jodimsprite.setImage(assets.image`PJ`)
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    if (swap == 0) {
+        animation.stopAnimation(animation.AnimationTypes.ImageAnimation, playersprite)
+        playersprite.setImage(assets.image`plaas`)
+    } else if (swap == 1) {
+        animation.stopAnimation(animation.AnimationTypes.ImageAnimation, playersprite)
+        animation.stopAnimation(animation.AnimationTypes.ImageAnimation, Jodimsprite)
+        Jodimsprite.setImage(assets.image`Jodim`)
+    }
+})
+statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
+    sprites.destroy(myEnemy, effects.spray, 500)
+    pause(500)
+    mySprite2.setFlag(SpriteFlag.Invisible, false)
+    playersprite.setPosition(160, 120)
+    mySprite2.setPosition(211, 120)
+    game.showLongText("well that guy is dead.", DialogLayout.Bottom)
+    game.showLongText("lets keep going.", DialogLayout.Bottom)
+    mySprite2.setFlag(SpriteFlag.Invisible, true)
+    tiles.setCurrentTilemap(tilemap`level10`)
+    tiles.placeOnTile(playersprite, tiles.getTileLocation(6, 15))
+    game.showLongText("it's night", DialogLayout.Bottom)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Jodim, function (sprite, otherSprite) {
+    if (controller.A.isPressed() && swap == 0) {
+        swap = 1
+        controller.moveSprite(playersprite, 0, 0)
+        playersprite.setKind(SpriteKind.Jodim)
+        controller.moveSprite(Jodimsprite)
+        Jodimsprite.setKind(SpriteKind.Player)
+        statusbar.attachToSprite(Jodimsprite)
+        scene.cameraFollowSprite(Jodimsprite)
+        pause(500)
+    } else if (controller.A.isPressed() && swap == 1) {
+        swap = 0
+        controller.moveSprite(playersprite)
+        playersprite.setKind(SpriteKind.Player)
+        controller.moveSprite(Jodimsprite, 0, 0)
+        Jodimsprite.setKind(SpriteKind.Jodim)
+        statusbar.attachToSprite(playersprite)
+        scene.cameraFollowSprite(playersprite)
+        pause(500)
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (start == 0 && swap == 0) {
+        animation.runImageAnimation(
+        playersprite,
+        assets.animation`plaaaaR`,
+        200,
+        true
+        )
+    } else if (start == 0 && swap == 1) {
+        animation.runImageAnimation(
+        Jodimsprite,
+        assets.animation`pJaaR`,
+        200,
+        true
+        )
+    }
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    pause(100)
+    animation.runImageAnimation(
+    myEnemy,
+    assets.animation`seaaL`,
+    200,
+    false
+    )
+    statusbar.value += -1
+    animation.runImageAnimation(
+    playersprite,
+    assets.animation`seaL`,
+    200,
+    true
+    )
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.villager2, function (sprite, otherSprite) {
+    if (controller.A.isPressed()) {
+        game.showLongText("watch out for the knights.", DialogLayout.Bottom)
+        pause(100)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`village`)
+    tiles.placeOnTile(playersprite, tiles.getTileLocation(1, 32))
+    Jodimsprite = sprites.create(assets.image`Jodim`, SpriteKind.Jodim)
+    tiles.placeOnTile(Jodimsprite, tiles.getTileLocation(31, 1))
+    mySprite = sprites.create(assets.image`asset1`, SpriteKind.villager1)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(31, 24))
+    mySprite.setScale(0.75, ScaleAnchor.Middle)
+    mySprite3 = sprites.create(assets.image`asset2`, SpriteKind.villager1)
+    tiles.placeOnTile(mySprite3, tiles.getTileLocation(13, randint(23, 24)))
+    mySprite3.setScale(0.75, ScaleAnchor.Middle)
+    mySprite4 = sprites.create(assets.image`asset3`, SpriteKind.villager1)
+    tiles.placeOnTile(mySprite4, tiles.getTileLocation(17, randint(23, 24)))
+    mySprite4.setScale(0.75, ScaleAnchor.Middle)
+    mySprite2.setFlag(SpriteFlag.Invisible, false)
+    tiles.placeOnTile(mySprite2, tiles.getTileLocation(27, randint(23, 24)))
+    mySprite2.setScale(0.75, ScaleAnchor.Middle)
+    mySprite5 = sprites.create(assets.image`asset4`, SpriteKind.villager2)
+    tiles.placeOnTile(mySprite5, tiles.getTileLocation(6, randint(23, 24)))
+    mySprite5.setScale(0.75, ScaleAnchor.Middle)
+    mySprite6 = sprites.create(assets.image`monkey0`, SpriteKind.vendor)
+    tiles.placeOnTile(mySprite6, tiles.getTileLocation(13, 31))
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.vendor, function (sprite, otherSprite) {
+    if (controller.A.isPressed()) {
+        game.showLongText("here have a banana. ", DialogLayout.Bottom)
+        bananas += 1
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.villager1, function (sprite, otherSprite) {
+    if (controller.A.isPressed()) {
+        game.showLongText("Hi!", DialogLayout.Bottom)
+        pause(100)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorLight4, function (sprite, location) {
+    mySprite2 = sprites.create(assets.image`talkity`, SpriteKind.talkity)
+    tiles.setCurrentTilemap(tilemap`level4`)
+    game.setDialogCursor(assets.image`tree`)
+    scene.setBackgroundImage(assets.image`bg2`)
+    game.setDialogFrame(assets.image`df`)
+    playersprite.setPosition(27, 75)
+    game.showLongText("you made it out!", DialogLayout.Bottom)
+    game.showLongText("we need to make it to a forest.", DialogLayout.Bottom)
+    game.showLongText("lets go!", DialogLayout.Bottom)
+    tiles.setCurrentTilemap(tilemap`level6`)
+    mySprite2.setFlag(SpriteFlag.Invisible, true)
+    tiles.placeOnTile(playersprite, tiles.getTileLocation(5, 15))
+    playersprite.setScale(0.75, ScaleAnchor.Middle)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (controller.B.isPressed()) {
+        statusbar2.value += randint(-1, -3)
+    }
+})
+let mySprite6: Sprite = null
+let mySprite5: Sprite = null
+let mySprite4: Sprite = null
+let mySprite3: Sprite = null
+let mySprite: Sprite = null
+let mySprite2: Sprite = null
+let open = 0
+let Jodimsprite: Sprite = null
+let statusbar: StatusBarSprite = null
+let statusbar2: StatusBarSprite = null
+let myEnemy: Sprite = null
+let textSprite2: TextSprite = null
+let textSprite: TextSprite = null
+let playersprite: Sprite = null
+let start = 0
+let swap = 0
+swap = 0
+start = 1
+let bananas = 0
+scene.setBackgroundImage(assets.image`bg`)
+playersprite = sprites.create(assets.image`p`, SpriteKind.Player)
+textSprite = textsprite.create("RPG")
+textSprite.setScale(2, ScaleAnchor.Middle)
+playersprite.setPosition(80, 60)
+scene.cameraFollowSprite(textSprite)
+textSprite2 = textsprite.create("press A to begin.")
+textSprite2.setPosition(93, 80)
+textSprite.setOutline(1, 2)
